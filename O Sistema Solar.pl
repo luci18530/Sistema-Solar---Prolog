1edac4f455a939bca9862a90d1d108257052165d
Luciano Pereira <54913266+luci18530@users.noreply.github.com>
2020-04-10 14:43:14 -0300

    Adição da massa dos corpos celestes e das suas funções relacionadas

--- O Sistema Solar.pl
+++ O Sistema Solar.pl
@@ -1,10 +1,15 @@
+% PROJETO DE LÓGICA APLICADA A COMPUTAÇÃO
+% GRUPO :
+% *Luciano Pereira de Oliveira Filho
+% *Lucas Issac Pissaia da Silva
+% *Vidal Elias Neto
 
+
+% Definição inicial, sol é uma estrela
 estrela(sol).
 
 
 % Relações de Orbitas.
-
-
 orbita(mercurio,sol).
 orbita(venus,sol).
 orbita(terra,sol).
@@ -47,74 +52,94 @@ orbita(hiaka,haumea).
 
 
 
-% Relações de raios.
+% Relações de dados de cada corpo celeste.
+% Função | Corpo | Raio em KM | Massa em KG
 
-raiokm(sol,696342).
-raiokm(mercurio,2439).
-raiokm(venus,6051).
-raiokm(terra,6371).
-raiokm(marte,3389).
-raiokm(jupiter,69911).
-raiokm(saturno,58232).
-raiokm(urano,25362).
-raiokm(netuno,24622).
-raiokm(ganymedes,2634).
-raiokm(titan,2574).
-raiokm(calisto,2410).
-raiokm(io,1821).
-raiokm(lua,1737).
-raiokm(europa,1560).
-raiokm(tritão,1353).
-raiokm(plutão,1188).
-raiokm(eris,1163).
-raiokm(titania,788).
-raiokm(haumea,780).
-raiokm(reia,763).
-raiokm(oberon,761).
-raiokm(japeto,734).
-raiokm(makemake,715).
-raiokm(caronte,606).
-raiokm(umbriel,584).
-raiokm(ariel,578).
-raiokm(dione,561).
-raiokm(gonggong,615).
-raiokm(quaoar,560).
-raiokm(sedna,498).
-raiokm(ceres,469).
-raiokm(disnomia,350).
-raiokm(proteu, 210).
-raiokm(nereida, 178).
-raiokm(larissa, 97).
-raiokm(fobos,11).
-raiokm(deimos,6).
+dados(sol,696342, 1.989*1E30).
+dados(mercurio,2439, 3.285*1E23).
+dados(venus,6051, 4.867*1E24).
+dados(terra,6371, 5.972*1E24).
+dados(marte,3389, 6.39*1E23).
+dados(jupiter,69911, 1.898*1E27).
+dados(saturno,58232, 5.683*1E26).
+dados(urano,25362, 8.681*1E25).
+dados(netuno,24622, 1.024*1E26).
+dados(ganymedes,2634, 1.4819 * 1E23 ).
+dados(titan,2574, 1.3452 * 1E23).
+dados(calisto,2410, 1.075938 * 1E23).
+dados(io,1821,9.9319 * 1E22).
+dados(lua,1737, 7.349 * 1E22).
+dados(europa,1560, 4.80 * 1E22).
+dados(tritão,1353, 2.14 * 1E22).
+dados(plutão,1188, 1.305 * 1E22).
+dados(eris,1163, 1.66 * 1E22).
+dados(titania,788, 3.527 * 1E21).
+dados(haumea,780, 4.2 * 1E21).
+dados(reia,763, 1.66 * 1E21).
+dados(oberon,761, 3.014 * 1E21).
+dados(japeto,734, 1.805 * 1E21).
+dados(makemake,715, 3.1 * 1E21).
+dados(caronte,606, 1.586 * 1E21).
+dados(umbriel,584, 1.275 * 1E21).
+dados(ariel,578, 1.251 * 1E21).
+dados(dione,561, 1.095 * 1E21).
+dados(gonggong,615, 1.75 * 1E21).
+dados(quaoar,560, 1.40 * 1E21).
+dados(sedna,4983, 2 * 1E21).
+dados(ceres,469, 8.958 * 1E20).
+dados(disnomia,350, 4 * 1E20).
+dados(proteu, 210, 4.4 * 1E19).
+dados(nereida, 178, 3.1 * 1E19).
+dados(larissa, 97, 4.2 * 1E18).
+dados(fobos,11, 1.08 * 1E16).
+dados(deimos,6, 1.4 * 1E15).
 
 
 % fonte: https://en.wikipedia.org/wiki/List_of_Solar_System_objects_by_size
 
 
+
+% Mostra a massa em kilogramas
+massa(CORPO,MASSA) :- dados(CORPO,_,MASSA).
+
+% Mossa a massa em massas terrestres
+massasterrestres(CORPO,MT) :- massa(CORPO, MASSA), MT is MASSA/(5.972*1E24).
+
+% Mostra o raio em kilômetros
+raio(CORPO,RAIO) :- dados(CORPO,RAIO,_).
+
 % Definição de um planeta
-planeta(P) :- orbita(P,S), estrela(S), raiokm(P, RAIO), RAIO > 1500.
+planeta(P) :- orbita(P,S), estrela(S), dados(P, RAIO), RAIO > 1500.
 
 % Definição de uma lua
 lua(L) :- orbita(L,P), (planeta(P); planeta_anao(P)).
 
 % Definição de um planeta anão
-planeta_anao(PA) :- orbita(PA,S), estrela(S), raiokm(PA, RAIO), RAIO < 1500.
+planeta_anao(PA) :- orbita(PA,S), estrela(S), dados(PA, RAIO), RAIO < 1500.
 
 % Definindo os planetas gasosos
-planetagasoso(PGAS) :- planeta(PGAS), raiokm(PGAS, RAIO), RAIO > 10000.
+planetagasoso(PGAS) :- planeta(PGAS), raio(PGAS, RAIO), RAIO > 10000.
 
 % Definindo um planeta terreste
 planetaterreste(PTER) :- planeta(PTER), (not(planetagasoso(PTER))).
 
 % Se um é maior que o outro
-maiorque(X,Y) :- raiokm(X,A), raiokm(Y,B), A>B.
+maiorque(X,Y) :- raio(X,A), raio(Y,B), A>B.
 
 % Diferença dos raios entre os corpos celestes
-diferenca_raiokm(P1,P2,DIF) :- raiokm(P1,A), raiokm(P2,B),  DIF is abs(A-B).
+diferenca_raio(P1,P2,DIF) :- raio(P1,A), raio(P2,B),  DIF is abs(A-B).
 
 % A proporção dos raios entre os corpos celestes
-proporcao_raiokm(P1,P2,PRO) :- raiokm(P1,A), raiokm(P2,B),  PRO is A/B.
+proporcao_raio(P1,P2,PRO) :- raio(P1,A), raio(P2,B),  PRO is A/B.
 
-% Volume do corpo celeste
-volumebikmcub(P,VOL) :- raiokm(P,RAIO), VOL is ((4*3.1415*RAIO^3)/3000000000).
+% Área da superficie de um corpo celeste em km²
+area(CORPO,AREA) :- raio(CORPO,RAIO), AREA is RAIO^2*4*pi.
+
+% Volume do corpo celeste em milhoes de kilometros cubicos
+volume(P,VOL) :- raio(P,RAIO), VOL is ((4*pi*RAIO^3)/3000000000).
+
+% Densidade de um corpo celeste em g/cm³
+densidade(CORPO,DEN) :-  volume(CORPO,VOL), massa(CORPO,MASSA), DEN is (MASSA*1000)/(VOL*1E24).
+
+% Gravidade de superficie de um corpo celeste em m/s²
+gravidade(CORPO,GRA) :- raio(CORPO,RAIO), massa(CORPO,MASSA), GRA is (6.674184*1E-11*MASSA)/((RAIO*1000)^2).
